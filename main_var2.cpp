226a0b574929611e6f4b120519c676c2c76dd00c
githubrobert12 <cretu_robert96@yahoo.com>
2016-12-19 10:52:34 +0200

    Update and rename main.cpp to main_var2.cpp

--- main_var2.cpp
+++ main_var2.cpp
@@ -11,8 +11,8 @@ class CVector {
  
  CVector operator + (CVector param) {
  CVector temp;
- temp.x = x + param.x;
- temp.y = y + param.y;
+ temp.x = x - param.x;
+ temp.y = y - param.y;
  return (temp);
 };
  
@@ -32,7 +32,7 @@ int main () {
  CVector b (1,2);
  CVector c;
  CVector d;
- c = a + b;
+ c = a - b;
  d = a;
  cout << c.x << "," << c.y<<endl;
  cout << d.x << "," << d.y<<endl;
